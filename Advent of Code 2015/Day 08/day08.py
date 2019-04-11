8fdb5695376dae710ae7bec62e6f8d95286b815c
Klyzx <15092850+Klyzx@users.noreply.github.com>
2019-04-11 00:02:40 -0700

    Create day08.py

---
+++ Advent of Code 2015/Day 08/day08.py
@@ -0,0 +1,30 @@
+stringliteral = 0
+stringmemory = 0
+stringnew = 0
+
+def sizeliteral(string):
+    return len(string)
+
+def sizememory(string):
+    return len(eval(string))
+
+def newstring(string):
+    string = string.replace("\\", "\\\\")
+    string = string.replace("\"", "\\\"")
+
+    print(string)
+    return (len(string) + 2)
+
+with open('input08.txt') as file:
+    for line in file:
+        line = line.rstrip()
+        stringliteral += sizeliteral(line)
+        stringmemory  += sizememory(line)
+        stringnew += newstring(line)
+
+
+print("Literal - Memory:   ", stringliteral - stringmemory)
+print("Literal - New:      ", stringnew - stringliteral)
+print("String literal:     ", stringliteral)
+print("String memory:      ", stringmemory)
+print("String new:         ", stringnew)
