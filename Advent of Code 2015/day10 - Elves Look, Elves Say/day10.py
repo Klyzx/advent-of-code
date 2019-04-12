fa905c7bfd3a3bf0f13f64b3f0d24a2b2850b264
Klyzx <15092850+Klyzx@users.noreply.github.com>
2019-04-11 21:58:59 -0700

    Create day10.py

---
+++ Advent of Code 2015/day10 - Elves Look, Elves Say/day10.py
@@ -0,0 +1,21 @@
+from itertools import groupby
+
+number = "1113222113"
+
+def newnumber(currentnum):
+    string = ""
+    grouped_data = groupby(number)
+    for key, grp in grouped_data:
+        string += str(len(list(grp)))
+        string += key
+    return string
+
+for i in range(0,40):
+    number = newnumber(number)
+number1 = number
+
+for i in range(0,10):
+    number = newnumber(number)
+
+print(len(number1))
+print(len(number))
