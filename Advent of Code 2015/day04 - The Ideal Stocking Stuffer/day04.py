f3cb8f4e7b9f755941f0ed0d244169c313c11ee4
Klyzx <15092850+Klyzx@users.noreply.github.com>
2019-11-30 23:03:48 -0800

    Update day04.py

--- Advent of Code 2015/day04 - The Ideal Stocking Stuffer/day04.py
+++ Advent of Code 2015/day04 - The Ideal Stocking Stuffer/day04.py
@@ -1,18 +1,13 @@
 from hashlib import md5
 thecode = 'yzbqklnj'
-integer = 0
 
 def hash(num):
-    global integer
-    global thecode
+    integer = 0
     while True:
         result = md5((thecode + str(integer)).encode()).hexdigest()
-        str(result)
         if result[:num] == '0'*num:
-            print(result)
-            print(integer)
-            break
+            return(integer)
         integer += 1
 
-hash(5)
-hash(6)
+print(hash(5))
+print(hash(6))
