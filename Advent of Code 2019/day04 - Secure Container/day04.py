3f03d869736a69469c04ad85760954b15034ba73
Klyzx <15092850+Klyzx@users.noreply.github.com>
2019-12-03 22:32:42 -0800

    Made day 4 look not gross

--- Advent of Code 2019/day04 - Secure Container/day04.py
+++ Advent of Code 2019/day04 - Secure Container/day04.py
@@ -1,29 +1,24 @@
 from itertools import groupby
 
-def checker(num):
-    consec = False
-    incr = True
-    num = str(num)
-    for x in range(len(num) - 1):
-        if num[x] == num[x + 1]:
-            consec = True
-        if num[x] > num[x + 1]:
-            incr = False
-    return consec and incr
-
-def newnumber(currentnum):
+def repetitions(num):
     string = ""
-    grouped_data = groupby(str(currentnum))
+    grouped_data = groupby(str(num))
     for _, grp in grouped_data:
         string += str(len(list(grp)))
     return string
 
+def checker(num):
+    repeat = repetitions(num)
+    if [x for x in str(num)] != sorted(str(num)):
+        return False
+    for x in repeat:
+        if int(x) >= 2:
+            return True
+    return False
+
 def checker2(num):
-    blank = ""
-    string = newnumber(num)
-    for x in range(len(string)):
-        blank += string[x]
-    for x in blank:
+    repeat = repetitions(num)
+    for x in repeat:
         if int(x) == 2:
             return True
     return False
@@ -33,10 +28,10 @@ maxr = 765747
 valid1 = 0
 valid2 = 0
 
-for num in range(minr, maxr + 1):
-    if checker(num):
+for x in range(minr, maxr + 1):
+    if checker(x):
         valid1 += 1
-        if checker2(num):
+        if checker2(x):
             valid2 += 1
 
 print(valid1, valid2)
