8b76f13a068e6512f26ef176a69696ca959b24ea
Klyzx <15092850+Klyzx@users.noreply.github.com>
2020-12-09 22:08:45 -0800

    Update day10.py

--- Advent of Code 2020/day10.py
+++ Advent of Code 2020/day10.py
@@ -9,27 +9,17 @@ def part1(voltages):
     diff_list = []
     for x, y in zip(voltages[0:], voltages[1:]):
         diff_list.append(y-x)
-    print(diff_list.count(1)*diff_list.count(3))
-
-
-def validAdapter(voltage, adapterList, adaptersChecked):
-    valid = 0
-    if voltage == 0:
-        return 1
-    for i in range(1, 4):
-        if voltage - i in adapterList:
-            if adaptersChecked[voltage - i]:
-                valid += adaptersChecked[voltage - i]
-                continue
-            valid += validAdapter(voltage - i, adapterList, adaptersChecked)
-        adaptersChecked[voltage] = valid
-    return valid
+    return diff_list.count(1)*diff_list.count(3)
 
 
 data.append(0)
 data.append(max(data)+3)
 data.sort()
 adapterDict = defaultdict(int)
+adapterDict[0] = 1
+for i in data[1:]:
+    adapterDict[i] = adapterDict[i-1] + adapterDict[i-2] + adapterDict[i-3]
 
-part1(data)
-validAdapter(max(data), data, adapterDict)
+
+print(f'Part 1: {part1(data)}')
+print(f'Part 2: {adapterDict[max(data)]}')
