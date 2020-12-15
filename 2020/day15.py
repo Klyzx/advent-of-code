30a2849bf686d949667136ec7a9284201af881b4
Klyzx <15092850+Klyzx@users.noreply.github.com>
2020-12-14 22:06:13 -0800

    Update day15.py

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,23 +1,21 @@
-arr = [15, 5, 1, 4, 7, 0]
+from collections import defaultdict
+
 
 def solver(timer):
-    spoken_numbers = {}
+    spoken_numbers = defaultdict(lambda: turn)
 
     for turn, number in enumerate(arr):
         spoken_numbers[number] = turn
         last_number = number
 
     while turn < timer - 1:
-        if last_number not in spoken_numbers:
-            next_number = 0
-        else:
-            next_number = turn - spoken_numbers[last_number]
-
+        next_number = turn - spoken_numbers[last_number]
         spoken_numbers[last_number] = turn
         last_number = next_number
         turn += 1
     return last_number
 
 
+arr = [15, 5, 1, 4, 7, 0]
 print(solver(2020))
 print(solver(30000000))
